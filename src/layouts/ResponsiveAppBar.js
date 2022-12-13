99a407fd10d3dc72a8db7d25cd132235970215f6
Nguyen Hoang Nguyen <hoangnguyen789a@gmail.com>
2022-12-13 11:21:32 +0700

    change logo title

--- src/layouts/ResponsiveAppBar.js
+++ src/layouts/ResponsiveAppBar.js
@@ -67,7 +67,7 @@ function ResponsiveAppBar() {
               textDecoration: "none",
             }}
           >
-            LOGO
+            NGUYEN KHOA
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
